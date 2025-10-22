6c1440c360662f857088168c9f77c74271a55e99
jamazATWIT <jamaz@wit.edu>
2025-10-22 11:10:21 -0400

    added client, portfolio, and security entity classes

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,56 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private Client client;
+    
+    @Column(nullable = false)
+    private LocalDate creationDate;
+    
+    @Column(nullable = false)
+    private List<Security> securities;
+    
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, LocalDate creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+        public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
